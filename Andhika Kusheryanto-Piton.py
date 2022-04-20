e4931a3ac108fac915dc828dbb69406ecacc7dfb
Zero Gride <69594776+zerogride@users.noreply.github.com>
2022-04-21 00:16:36 +0700

    Create Andhika Kusheryanto-Piton.py

---
+++ Andhika Kusheryanto-Piton.py
@@ -0,0 +1,17 @@
+print("nama : andhika kusheryanto")
+print("nim : 191011402130")
+#semuanya aja ya saya kerjain sekalian :v
+i=25
+print("If Else")
+if i>10:
+print("i  lebbihh dari 10")
+else:
+print("i  kurang dari  10")
+
+print("")
+print("while")
+while i>0
+i +=1
+print("i=",i)
+if(i==30):#Max 30x loop
+break
